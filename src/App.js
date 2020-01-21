7fb08eaf40cbac814d904dc4ed7012fc3cd24a7c
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-21 15:59:47 -0300

    criando os cards dos usuarios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,101 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
 function App() {
   return (
-    <h1>hello</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+         
+         <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/3675462?s=460&v=4" alt="Iarlen Aquiles"/>
+
+              <div className="user-info">
+                <strong>Iarlen Aquiles</strong>
+                <span>ReactJs, NodeJs</span>
+              </div>
+            </header>
+            <p>dfdfdf dfdfd fdf dfdf dfdf dfd</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/3675462?s=460&v=4" alt="Iarlen Aquiles"/>
+
+              <div className="user-info">
+                <strong>Iarlen Aquiles</strong>
+                <span>ReactJs, NodeJs</span>
+              </div>
+            </header>
+            <p>dfdfdf dfdfd fdf dfdf dfdf dfd</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/3675462?s=460&v=4" alt="Iarlen Aquiles"/>
+
+              <div className="user-info">
+                <strong>Iarlen Aquiles</strong>
+                <span>ReactJs, NodeJs</span>
+              </div>
+            </header>
+            <p>dfdfdf dfdfd fdf dfdf dfdf dfd</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/3675462?s=460&v=4" alt="Iarlen Aquiles"/>
+
+              <div className="user-info">
+                <strong>Iarlen Aquiles</strong>
+                <span>ReactJs, NodeJs</span>
+              </div>
+            </header>
+            <p>dfdfdf dfdfd fdf dfdf dfdf dfd</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+        </ul>
+
+      </main>
+    </div>
   );
 }
 
